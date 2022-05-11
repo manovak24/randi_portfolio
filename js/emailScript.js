18b2c5642d740654e9fd3155a47c6ec7938a55de
emagdnimMQT <mark.w.novak@gmail.com>
2022-05-11 11:33:58 -0400

    Refracted email script.

--- js/emailScript.js
+++ js/emailScript.js
@@ -4,12 +4,19 @@ const userMessage = document.getElementById("message");
 const formButton = document.getElementById("submit-btn");
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-const formValidate = () => {
-	const name = fullName.value;
-	const email = userEmail.value;
-	const message = userMessage.value;
+(function() {
+	emailjs.init("user_rQF8cs4fYnfBs2O7qJ0gs");
+	})();
+  
+function sendEmail() {
+	// contact parameters is required for the emailjs.send function
+	const contactParams = {
+		name: fullName.value,
+		email: userEmail.value,
+		message: userMessage.value
+	};
 
-	if (name.length < 3) {
+	if (fullName.value.length < 3) {
 		Swal.fire({
 			title: 'Error!',
 			text: 'Please enter a valid name',
@@ -19,9 +26,7 @@ const formValidate = () => {
       		color: '#151515'
 		});
 		return false;
-	}
-
-	if(!email.match(emailRegex)) {
+	} else if(!userEmail.value.match(emailRegex)) {
 		Swal.fire({
 		title: 'Error!',
 				text: 'Please enter a valid email',
@@ -31,9 +36,7 @@ const formValidate = () => {
       			color: '#151515'
 		})
 		return false
-	}
-
-	if(message < 3) {
+	} else if(userMessage.value.length < 3) {
 		Swal.fire({
 		title: 'Error!',
 				text: 'Please enter a valid message',
@@ -43,22 +46,7 @@ const formValidate = () => {
       			color: '#151515'
 		})
 		return false
-	}
-	return true
-}
-
-
-(function() {
-	emailjs.init("user_rQF8cs4fYnfBs2O7qJ0gs");
-	})();
-  
-function sendEmail() {
-	var contactParams = {
-		user_name: fullName.value,
-		user_email: userEmail.value,
-		message: userMessage.value
-	};
-	if (formValidate()) {
+	} else {
 		emailjs.send('gmail', 'portfolio', contactParams).then(function (res) {})
 		Swal.fire({
 		title: 'Success!',
